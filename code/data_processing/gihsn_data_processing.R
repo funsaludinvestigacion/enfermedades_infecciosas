82d61f9cf1f0f28afc2d233547d729d65596cf29
Julio del Cid <juliodelcidvillatoro@gmail.com>
2025-07-09 14:57:46 -0600

    agregando_resultados_del_ABI

--- code/data_processing/gihsn_data_processing.R
+++ code/data_processing/gihsn_data_processing.R
@@ -56,20 +56,29 @@ gihsn_results <- gihsn %>%
   summarize(
     total_tested = n_distinct(record_id),  # Count unique IDs tested per epiweek
     total_pos = n_distinct(record_id[sars_cov2_final == 1 | inf_a_final == 1 | 
-                                       inf_b_final == 1 | vsr_final == 1], na.rm = TRUE),
-    total_neg = n_distinct(record_id[sars_cov2_final == 2 & inf_a_final == 2 & 
-                                       inf_b_final == 2 & vsr_final == 2], na.rm = TRUE),
-    sars_cov2_pos = sum(sars_cov2_final == 1, na.rm = TRUE),
-    sars_cov2_neg = sum(sars_cov2_final == 2, na.rm = TRUE),
-    inf_a_pos = sum(inf_a_final == 1, na.rm = TRUE),
-    inf_a_neg = sum(inf_a_final == 2, na.rm = TRUE),
-    inf_b_pos = sum(inf_b_final == 1, na.rm = TRUE),
-    inf_b_neg = sum(inf_b_final == 2, na.rm = TRUE),
+                                       inf_b_final == 1 | vsr_final == 1|inf_a_abi==1|
+                                       inf_b_abi==1|sars_cov2_abi==1], na.rm = TRUE),
+    total_neg = n_distinct(record_id[(sars_cov2_final == 2 & inf_a_final == 2 & 
+                                       inf_b_final == 2 & vsr_final == 2)|(inf_a_abi==1&
+                                                                             inf_b_abi==1&sars_cov2_abi==1)], na.rm = TRUE),
+    sars_cov2_pos = sum(sars_cov2_final == 1, na.rm = TRUE)+
+      sum(sars_cov2_abi==1, na.rm = TRUE),
+    sars_cov2_neg = sum(sars_cov2_final == 2, na.rm = TRUE)+
+      sum(sars_cov2_abi == 2, na.rm = TRUE),
+    inf_a_pos = sum(inf_a_final == 1, na.rm = TRUE)+
+      sum(inf_a_abi == 1, na.rm = TRUE),
+    inf_a_neg = sum(inf_a_final == 2, na.rm = TRUE)+
+      sum(inf_a_abi == 2, na.rm = TRUE),
+    inf_b_pos = sum(inf_b_final == 1, na.rm = TRUE)+
+      sum(inf_b_abi == 1, na.rm = TRUE),
+    inf_b_neg = sum(inf_b_final == 2, na.rm = TRUE)+
+      sum(inf_b_abi == 2, na.rm = TRUE),
     vsr_pos = sum(vsr_final == 1, na.rm = TRUE),
     vsr_neg = sum(vsr_final == 2, na.rm = TRUE),
     inf_a_h1n1 = sum(subtipo_infa == "H1N1", na.rm = TRUE),
     inf_a_h3n2 = sum(subtipo_infa == "H3N2", na.rm = TRUE),
-    inf_a_nosub = sum(inf_a == 1 & is.na(subtipo_infa), na.rm = TRUE)
+    inf_a_nosub = sum(inf_a_final == 1 & is.na(subtipo_infa), na.rm = TRUE)+
+      sum(inf_a_abi == 1 & is.na(subtipo_infa), na.rm = TRUE)
   ) %>%
   arrange(year, epiweek)  # Optionally, sort by year and epiweek
 
